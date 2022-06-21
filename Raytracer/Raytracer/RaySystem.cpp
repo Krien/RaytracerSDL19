b5c371c4748ad7d2e70b2d77c7d9022f09a4420c
Tim Schagen <schagen34@gmail.com>
2022-06-21 22:32:25 +0200

    Move draw to raysystem

--- Raytracer/Raytracer/RaySystem.cpp
+++ Raytracer/Raytracer/RaySystem.cpp
@@ -1,81 +1,85 @@
 #include "precomp.h"
-
+#include "xmmintrin.h"
 #define SIZE SCREEN_WIDTH * SCREEN_HEIGHT
 
-RaySystem::RaySystem(Scene* scene)
+RaySystem::RaySystem(Screen* screen)
 {
-	RaySystem::scene = scene;
+
+	this->width = screen->getWidth();
+	this->height = screen->getHeight();
 }
 
-Vec3Df* RaySystem::trace()
-{
-	__m256 rx8[RAY_COUNT];
-	__m256 ry8[RAY_COUNT];
-	__m256 rz8[RAY_COUNT];
+void RaySystem::init(Scene* scene, Camera* camera) {
+	this->scene = scene;
+	this->camera = camera;
+}
 
-	Vec3Df* traceResult = new Vec3Df[SIZE];
-	for (int i = 0; i < SIZE / 8; i++)
+void RaySystem::draw(Pixel* pixelBuffer) {
+	Vec3Df startDir = camera->getRelTopLeft();
+	Vec3Df camPos = camera->position;
+	const static float xOffset = (float)SCREEN_DIMENSION * 2 / (width - 1);
+	const static float yOffset = (float)SCREEN_DIMENSION * 2 / (height - 1);
+
+	const __m256 startX = _mm256_set1_ps(startDir.get_x());
+	const __m256 startY = _mm256_set1_ps(startDir.get_y());
+	const __m256 startZ = _mm256_set1_ps(startDir.get_z());
+
+	const __m256 originX = _mm256_set1_ps(camPos.get_x());
+	const __m256 originY = _mm256_set1_ps(camPos.get_y());
+	const __m256 originZ = _mm256_set1_ps(camPos.get_z());
+
+	const __m256 rayLen = _mm256_set1_ps(100);
+
+	// #pragma unroll
+	for (unsigned int i = 0; i < (height * width) / AVX_SIZE; i++)
 	{
-		traceResult[i * 8    ] = Vec3Df(rx8[i].m256_f32[0], ry8[i].m256_f32[0], rz8[i].m256_f32[0]);
-		traceResult[i * 8 + 1] = Vec3Df(rx8[i].m256_f32[1], ry8[i].m256_f32[1], rz8[i].m256_f32[1]);
-		traceResult[i * 8 + 2] = Vec3Df(rx8[i].m256_f32[2], ry8[i].m256_f32[2], rz8[i].m256_f32[2]);
-		traceResult[i * 8 + 3] = Vec3Df(rx8[i].m256_f32[3], ry8[i].m256_f32[3], rz8[i].m256_f32[3]);
-		traceResult[i * 8 + 4] = Vec3Df(rx8[i].m256_f32[4], ry8[i].m256_f32[4], rz8[i].m256_f32[4]);
-		traceResult[i * 8 + 5] = Vec3Df(rx8[i].m256_f32[5], ry8[i].m256_f32[5], rz8[i].m256_f32[5]);
-		traceResult[i * 8 + 6] = Vec3Df(rx8[i].m256_f32[6], ry8[i].m256_f32[6], rz8[i].m256_f32[6]);
-		traceResult[i * 8 + 7] = Vec3Df(rx8[i].m256_f32[7], ry8[i].m256_f32[7], rz8[i].m256_f32[7]);
+		int x = i * AVX_SIZE % width;
+		int y = i * AVX_SIZE / width;
+
+		__m256 dirX = _mm256_add_ps(startX,
+			_mm256_set_ps(x * xOffset, x + 1 * xOffset, x + 2 * xOffset, x + 3 * xOffset, x + 4 * xOffset, x + 5 * xOffset, x + 6 * xOffset, x + 7 * xOffset));
+		__m256 dirY = _mm256_add_ps(startY,
+			_mm256_set_ps(y * yOffset, y + 1 * yOffset, y + 2 * yOffset, y + 3 * yOffset, y + 4 * yOffset, y + 5 * yOffset, y + 6 * yOffset, y + 7 * yOffset));
+		__m256 dirZ = _mm256_setzero_ps();
+
+		__m256* argb = trace(dirX, dirY, dirZ, originX, originY, originZ, 0);
+		// Convert color, but since I dunno what the trace function will return im leaving this empty forn ow
 	}
 
-	return traceResult;
+	/*
+	Vec3Df rayStartDir = camera->getRelTopLeft();
+	float xOffset = (float)SCREEN_DIMENSION * 2 / (width - 1);
+	float yOffset = (float)SCREEN_DIMENSION * 2 / (height - 1);
+	#pragma unroll
+	for (unsigned int y = 0; y < height; y++)
+	{
+		for (unsigned int x = 0; x < width; x++)
+		{
+			// Make ray
+			Vec3Df direction = rayStartDir + Vec3Df(x * xOffset, y * yOffset, 0);
+			direction = normalize_vector(direction);
+			Ray r = { camera->position, direction, 100 };
+			// Check for hits
+			Vec3Df argb = trace(r,0) * Vec3Df(255);
+
+			// Convert color
+			unsigned int xy = x * 4 + (y * width) * 4;
+			*(pixelBuffer + xy) = std::min((int)argb.extract(0), 255);
+			*(pixelBuffer + xy+1) = std::min((int)argb.extract(1), 255);
+			*(pixelBuffer + xy+2) = std::min((int)argb.extract(2), 255);
+			*(pixelBuffer + xy + 3) = 0;
+		}
+	};*/
+}
+
+
+// No idea what this should return, but probably a colorX, colorY and colorZ thats why its returning a __m256 array 
+__m256* RaySystem::trace(__m256 rayDirX, __m256 rayDirY, __m256 rayDirZ, __m256 originX, __m256 originY, __m256 originZ, int depth)
+{
+	return NULL;
 }
 
 HitInfo* RaySystem::hit()
 {
-	// hit info
-	__m256 hit_nx8[RAY_COUNT];
-	__m256 hit_ny8[RAY_COUNT];
-	__m256 hit_nz8[RAY_COUNT];
-	__m256 hit_px8[RAY_COUNT];
-	__m256 hit_py8[RAY_COUNT];
-	__m256 hit_pz8[RAY_COUNT];
-	__m256 hit_dist8[RAY_COUNT];
-	__m256i hit_mat8[RAY_COUNT];
-	__m256i hit_id8[RAY_COUNT];
-
-	std::vector<Shape*> shapes = scene->objects;
-	for (size_t i = 0; i < shapes.size(); i++)
-	{
-		
-	}
-
-	HitInfo* hitResult = new HitInfo[SIZE];
-	for (int i = 0; i < SIZE / 8; i++)
-	{
-		hitResult[i * 8    ] = HitInfo{ Vec3Df(hit_nx8[i].m256_f32[0], hit_ny8[i].m256_f32[0], hit_nz8[i].m256_f32[0]),
-									   Vec3Df(hit_px8[i].m256_f32[0], hit_py8[i].m256_f32[0], hit_pz8[i].m256_f32[0]),
-									   hit_dist8[i].m256_f32[0], Shape::getMat(hit_mat8[i].m256i_i32[0]), hit_id8[i].m256i_i32[0] };
-		hitResult[i * 8 + 1] = HitInfo{ Vec3Df(hit_nx8[i].m256_f32[1], hit_ny8[i].m256_f32[1], hit_nz8[i].m256_f32[1]),
-									   Vec3Df(hit_px8[i].m256_f32[1], hit_py8[i].m256_f32[1], hit_pz8[i].m256_f32[1]),
-									   hit_dist8[i].m256_f32[1], Shape::getMat(hit_mat8[i].m256i_i32[1]), hit_id8[i].m256i_i32[1] };
-		hitResult[i * 8 + 2] = HitInfo{ Vec3Df(hit_nx8[i].m256_f32[2], hit_ny8[i].m256_f32[2], hit_nz8[i].m256_f32[2]),
-									   Vec3Df(hit_px8[i].m256_f32[2], hit_py8[i].m256_f32[2], hit_pz8[i].m256_f32[2]),
-									   hit_dist8[i].m256_f32[2], Shape::getMat(hit_mat8[i].m256i_i32[2]), hit_id8[i].m256i_i32[2] };
-		hitResult[i * 8 + 3] = HitInfo{ Vec3Df(hit_nx8[i].m256_f32[3], hit_ny8[i].m256_f32[3], hit_nz8[i].m256_f32[3]),
-									   Vec3Df(hit_px8[i].m256_f32[3], hit_py8[i].m256_f32[3], hit_pz8[i].m256_f32[3]),
-									   hit_dist8[i].m256_f32[3], Shape::getMat(hit_mat8[i].m256i_i32[3]), hit_id8[i].m256i_i32[3] };
-		hitResult[i * 8 + 4] = HitInfo{ Vec3Df(hit_nx8[i].m256_f32[4], hit_ny8[i].m256_f32[4], hit_nz8[i].m256_f32[4]),
-									   Vec3Df(hit_px8[i].m256_f32[4], hit_py8[i].m256_f32[4], hit_pz8[i].m256_f32[4]),
-									   hit_dist8[i].m256_f32[4], Shape::getMat(hit_mat8[i].m256i_i32[4]), hit_id8[i].m256i_i32[4] };
-		hitResult[i * 8 + 5] = HitInfo{ Vec3Df(hit_nx8[i].m256_f32[5], hit_ny8[i].m256_f32[5], hit_nz8[i].m256_f32[5]),
-									   Vec3Df(hit_px8[i].m256_f32[5], hit_py8[i].m256_f32[5], hit_pz8[i].m256_f32[5]),
-									   hit_dist8[i].m256_f32[5], Shape::getMat(hit_mat8[i].m256i_i32[5]), hit_id8[i].m256i_i32[5] };
-		hitResult[i * 8 + 6] = HitInfo{ Vec3Df(hit_nx8[i].m256_f32[6], hit_ny8[i].m256_f32[6], hit_nz8[i].m256_f32[6]),
-									   Vec3Df(hit_px8[i].m256_f32[6], hit_py8[i].m256_f32[6], hit_pz8[i].m256_f32[6]),
-									   hit_dist8[i].m256_f32[6], Shape::getMat(hit_mat8[i].m256i_i32[6]), hit_id8[i].m256i_i32[6] };
-		hitResult[i * 8 + 7] = HitInfo{ Vec3Df(hit_nx8[i].m256_f32[7], hit_ny8[i].m256_f32[7], hit_nz8[i].m256_f32[7]),
-									   Vec3Df(hit_px8[i].m256_f32[7], hit_py8[i].m256_f32[7], hit_pz8[i].m256_f32[7]),
-									   hit_dist8[i].m256_f32[7], Shape::getMat(hit_mat8[i].m256i_i32[7]), hit_id8[i].m256i_i32[7] };
-	}
-
-	return hitResult;
+	return NULL;
 }
--- Raytracer/Raytracer/RaySystem.h
+++ Raytracer/Raytracer/RaySystem.h
@@ -1,24 +1,28 @@
 #pragma once
+#include "xmmintrin.h"
 
 class RaySystem
 {
 public:
-	RaySystem(Scene* scene);
-
-	const static int RAY_COUNT = SCREEN_WIDTH * SCREEN_WIDTH / 8;
-
-	__m256 ox8[RAY_COUNT];
+	RaySystem(Screen* screen);
+	
+	const static int AVX_SIZE = 8;
+	/*__m256 ox8[RAY_COUNT];
 	__m256 oy8[RAY_COUNT];
 	__m256 oz8[RAY_COUNT];
 	__m256 dx8[RAY_COUNT];
 	__m256 dy8[RAY_COUNT];
 	__m256 dz8[RAY_COUNT];
 	__m256 len8[RAY_COUNT];
-	__m256i depth8[RAY_COUNT];
-
-	Vec3Df* trace();
+	__m256i depth8[RAY_COUNT];*/
+	void draw(Pixel* pixelBuffer);
+	void init(Scene* scene, Camera* camera);
+	__m256* trace(__m256 rayDirX, __m256 rayDirY, __m256 rayDirZ, __m256 originX, __m256 originY, __m256 originZ, int depth);
 	HitInfo* hit();
 
 private:
 	Scene* scene = NULL;
+	Camera* camera = NULL;
+	int width;
+	int height;
 };
