e09c3cffa5200d6e9912f98bbb3f0fa132cb786b
Tim Schagen <schagen34@gmail.com>
2022-06-23 22:33:31 +0200

    Sphere early out

--- Raytracer/Raytracer/Sphere.cpp
+++ Raytracer/Raytracer/Sphere.cpp
@@ -52,13 +52,37 @@ void Sphere::hit(Ray8 r, HitInfo8* hit) {
 	__m256 cy = _mm256_sub_ps(posY, r.oy);
 	__m256 cz = _mm256_sub_ps(posZ, r.oz); 
 	
-	__m256 cLenMask = _mm256_cmp_ps(dot_product(cx, cy, cz), radiusSq8, _CMP_LT_OQ);
+	/*float t = dot_product(c, r.direction);
+	Vec3Df q = c - Vec3Df(t) * r.direction;
+	float pSquared = dot_product(q, q);*/
+	__m256 t = dot_product(cx, cy, cz, r.dx, r.dy, r.dz);
+	__m256 qx = _mm256_sub_ps(cx, _mm256_mul_ps(r.dx, t));
+	__m256 qy = _mm256_sub_ps(cy, _mm256_mul_ps(r.dy, t));
+	__m256 qz = _mm256_sub_ps(cz, _mm256_mul_ps(r.dz, t));
+	__m256 pSquared = dot_product(qx, qy, qz);
+	t = _mm256_sub_ps(t, _mm256_sqrt_ps(_mm256_sub_ps(radiusSq8, pSquared)));
 	
 	// if (cLenSq < radiusSq)
+	__m256 cLenMask = _mm256_cmp_ps(dot_product(cx, cy, cz), radiusSq8, _CMP_LT_OQ);
 	// if (radius < r.length) (inside sphere)
-	HitInfo8 sphereHitInfo = HitInfo8();
 	__m256 shortMask = _mm256_cmp_ps(radius8, r.len, _CMP_LT_OQ);
 	__m256 inSphereMask = _mm256_and_ps(cLenMask, shortMask);
+	
+	// if (pSquared <= radiusSq)
+	__m256 checkHitMask = _mm256_cmp_ps(pSquared, radiusSq8, _CMP_LE_OQ);
+	// 	if ((t > 0) && (t < hit->distance)) 
+	__m256 withinEyeAndDistMask = _mm256_and_ps(_mm256_cmp_ps(t, _mm256_set1_ps(0.0f), _CMP_GT_OQ), _mm256_cmp_ps(t, hit->dist, _CMP_LT_OQ));
+	__m256 outsideHitMask = _mm256_and_ps(checkHitMask, withinEyeAndDistMask);
+
+	// we precalc all masks in hope of an early out before doing expensive stuff
+	__m256 completeHitMask = _mm256_or_ps(inSphereMask, outsideHitMask);
+	
+	if (_mm256_movemask_ps(completeHitMask) == 0) {
+		return;
+	}
+	HitInfo8 sphereHitInfo = HitInfo8();
+	
+
 	__m256 hitPosX = _mm256_add_ps(r.ox, _mm256_mul_ps(r.dx, radius8));
 	__m256 hitPosY = _mm256_add_ps(r.oy, _mm256_mul_ps(r.dy, radius8));
 	__m256 hitPosZ = _mm256_add_ps(r.oz, _mm256_mul_ps(r.dz, radius8));
@@ -75,19 +99,10 @@ void Sphere::hit(Ray8 r, HitInfo8* hit) {
 	
 	// else (outside sphere)
 	
-	/*float t = dot_product(c, r.direction);
-	Vec3Df q = c - Vec3Df(t) * r.direction;
-	float pSquared = dot_product(q, q);*/
-	__m256 t = dot_product(cx, cy, cz, r.dx, r.dy, r.dz);
-	__m256 qx = _mm256_sub_ps(cx, _mm256_mul_ps(r.dx, t));
-	__m256 qy = _mm256_sub_ps(cy, _mm256_mul_ps(r.dy, t));
-	__m256 qz = _mm256_sub_ps(cz, _mm256_mul_ps(r.dz, t));
-	__m256 pSquared = dot_product(qx, qy, qz);
-	__m256 checkHitMask = _mm256_cmp_ps(pSquared, radiusSq8, _CMP_LE_OQ);
-	t = _mm256_sub_ps(t, _mm256_sqrt_ps(_mm256_sub_ps(radiusSq8, pSquared)));
-	// 	if ((t > 0) && (t < hit->distance)) 
-	__m256 withinEyeAndDistMask = _mm256_and_ps(_mm256_cmp_ps(t, _mm256_set1_ps(0.0f), _CMP_GT_OQ), _mm256_cmp_ps(t, hit->dist, _CMP_LT_OQ));
-	__m256 outsideHitMask = _mm256_and_ps(checkHitMask, withinEyeAndDistMask);
+	
+	
+	
+
 	//Vec3Df hitPos = r.origin + r.direction * Vec3Df(t);
 	//Vec3Df normal = normalize_vector(hitPos - position);
 	//*hit = HitInfo{ normal,hitPos,t,mat, id };
@@ -103,10 +118,7 @@ void Sphere::hit(Ray8 r, HitInfo8* hit) {
 	sphereHitInfo.pz = _mm256_blendv_ps(sphereHitInfo.pz, hitPosZ, outsideHitMask);
 	sphereHitInfo.dist = _mm256_blendv_ps(sphereHitInfo.dist, t, outsideHitMask); 
 	
-	// Ok we now have the correct spherehitinfo (hopefully)
-	// so now we must check if we actually hit the sphere at all for the final result
-	__m256 completeHitMask = _mm256_or_ps(inSphereMask, outsideHitMask);
-
+	int test = _mm256_movemask_ps(completeHitMask);
 	hit->px = _mm256_blendv_ps(hit->px, sphereHitInfo.px, completeHitMask);
 	hit->py = _mm256_blendv_ps(hit->py, sphereHitInfo.py, completeHitMask);
 	hit->pz = _mm256_blendv_ps(hit->pz, sphereHitInfo.pz, completeHitMask);
