6611f5af2beb9ff143f4888ac9f62197a69958a3
Tim Schagen <schagen34@gmail.com>
2022-06-23 17:49:54 +0200

    slight speed improvement by storing avx in light

--- Raytracer/Raytracer/Light.cpp
+++ Raytracer/Raytracer/Light.cpp
@@ -4,4 +4,8 @@ Light::Light(Vec3Df position, Vec3Df intensity)
 {
 	Light::position = position;
 	Light::intensity = intensity;
+	Light::posX = _mm256_set1_ps(position.get_x());
+	Light::posY = _mm256_set1_ps(position.get_y());
+	Light::posZ = _mm256_set1_ps(position.get_z());
+	Light::intensity8 = _mm256_set1_ps(intensity.get_x());
 }
